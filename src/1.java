c12dace1cb60ebb9ee6277de4acd523b2de2c773
legginsgambale7 <legginsgambale7@gmail.com>
2025-03-06 09:29:44 +0000

    Add 1.java

---
+++ src/1.java
@@ -0,0 +1,12 @@
+public class MathProject {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 3;
+        int z = add(x, y);
+        System.out.println("The sum of " + x + " and " + y + " is: " + z);
+    }
+    
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
