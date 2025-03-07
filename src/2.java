6c6c5bf86debbf54ab4a33dbd5b90343a65dc9af
legginsgambale7 <legginsgambale7@gmail.com>
2025-03-07 19:18:04 +0000

    Add 2.java

---
+++ src/2.java
@@ -0,0 +1,21 @@
+public class MathOperations {
+  public static int sum(int a, int b) {
+    return a + b;
+  }
+  
+  public static int difference(int a, int b) {
+    return a - b;
+  }
+  
+  public static int product(int a, int b) {
+    return a * b;
+  }
+  
+  public static double quotient(double a, double b) {
+    return a / b;
+  }
+  
+  public static double remainder(double a, double b) {
+    return a % b;
+  }
+}
