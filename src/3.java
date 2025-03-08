53f0601f417ada4215094647c9d3bfdbbbd7c723
legginsgambale7 <legginsgambale7@gmail.com>
2025-03-08 21:32:06 +0000

    Add 3.java

---
+++ src/3.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class MathProject {
+    public static void main(String[] args) {
+        // Initialize variables
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        // Perform mathematical operations
+        int sum = a + b + c;
+        int difference = a - b - c;
+        int product = a * b * c;
+        double quotient = (double) a / b / c;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+    }
+}
