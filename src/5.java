0d910cb6d9f003d08c1cf52fc16eb78fb2af0a41
legginsgambale7 <legginsgambale7@gmail.com>
2025-03-10 00:27:27 +0000

    Add 5.java

---
+++ src/5.java
@@ -0,0 +1,22 @@
+// Java program to demonstrate basic arithmetic operators
+class ArithmeticOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        // Print sum of a and b using + operator
+        System.out.println("Sum: " + (a + b));
+
+        // Print difference of a and b using - operator
+        System.out.println("Difference: " + (a - b));
+
+        // Print product of a and b using * operator
+        System.out.println("Product: " + (a * b));
+
+        // Print quotient of a and b using / operator
+        System.out.println("Quotient: " + (a / b));
+
+        // Print remainder of a and b using % operator
+        System.out.println("Remainder: " + (a % b));
+    }
+}
