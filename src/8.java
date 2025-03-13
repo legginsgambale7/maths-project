18e901e1a797766b0c06a066336e1012cbf72b22
legginsgambale7 <legginsgambale7@gmail.com>
2025-03-13 10:34:58 +0000

    Add 8.java

---
+++ src/8.java
@@ -0,0 +1,10 @@
+public class MathProject {
+  public static void main(String[] args) {
+    int result = add(5, 2);
+    System.out.println("The sum of 5 and 2 is: " + result);
+  }
+  
+  private static int add(int a, int b) {
+    return a + b;
+  }
+}
