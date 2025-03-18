47a8bdff9e1691293a85c1c35a9837ffc2b278a5
legginsgambale7 <legginsgambale7@gmail.com>
2025-03-18 09:41:14 +0000

    Add 13.java

---
+++ src/13.java
@@ -0,0 +1,17 @@
+public class MathUtils {
+    public static int add(int x, int y) {
+        return x + y;
+    }
+
+    public static int subtract(int x, int y) {
+        return x - y;
+    }
+
+    public static int multiply(int x, int y) {
+        return x * y;
+    }
+
+    public static double divide(double x, double y) {
+        return x / y;
+    }
+}
