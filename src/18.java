666d4f8b976b66ba69e731252b950abfa8ea309d
legginsgambale7 <legginsgambale7@gmail.com>
2025-03-24 00:26:52 +0000

    Add 18.java

---
+++ src/18.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // Example code
+        System.out.println("Hello, World!");
+        
+        // Another example
+        String str = "Programming is fun!";
+        int num1 = 5;
+        double decimalNum = 3.141592653589793;
+        float floatNum = 0f;
+        
+        System.out.println("String: " + str);
+        System.out.println("Integer: " + num1);
+        System.out.println("Double: " + decimalNum);
+        System.out.println("Float: " + floatNum);
+    }
+}
