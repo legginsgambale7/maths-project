3fd77b227d92afd1408d47b3983293f52207939e
legginsgambale7 <legginsgambale7@gmail.com>
2025-03-30 16:26:27 +0000

    Add 21.java

---
+++ src/21.java
@@ -0,0 +1,12 @@
+public class MathProject {
+    public static void main(String[] args) {
+        // Example: Add two numbers
+        int num1 = 5;
+        int num2 = 3;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + (num1 + num2));
+        
+        // Example: Subtract two numbers
+        double result = Math.sqrt(4) - Math.sqrt(9);
+        System.out.println("The square root of 4 minus the square root of 9 is " + result);
+    }
+}
