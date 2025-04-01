db8e2a1ca8cc32f1ecb1aa51041565b552339810
legginsgambale7 <legginsgambale7@gmail.com>
2025-04-01 17:44:20 +0000

    Add 23.java

---
+++ src/23.java
@@ -0,0 +1,9 @@
+public class MathOperations {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int sum = 5 + 3;
+        double difference = 10.0 - 4.0;
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+    }
+}
