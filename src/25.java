a857b0fe8dd4168709e9fa2e1de90bc4d3397bbb
legginsgambale7 <legginsgambale7@gmail.com>
2025-04-03 19:09:12 +0000

    Add 25.java

---
+++ src/25.java
@@ -0,0 +1,19 @@
+public class MathOperations {
+    public static void main(String[] args) {
+        // Addition operation
+        int addition = 10 + 20;
+        System.out.println("The sum of 10 and 20 is: " + addition);
+
+        // Subtraction operation
+        int subtraction = 5 - 3;
+        System.out.println("The difference between 5 and 3 is: " + subtraction);
+
+        // Multiplication operation
+        int multiplication = 7 * 8;
+        System.out.println("The product of 7 and 8 is: " + multiplication);
+
+        // Division operation
+        double division = 10.0 / 2.0;
+        System.out.println("The quotient of 10.0 divided by 2.0 is: " + division);
+    }
+}
