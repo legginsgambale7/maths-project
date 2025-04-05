f86cfe0b7d1d1da428ba774d8be7634d6215e5bf
legginsgambale7 <legginsgambale7@gmail.com>
2025-04-05 05:55:21 +0000

    Add 26.java

---
+++ src/26.java
@@ -0,0 +1,8 @@
+public class MathOperations {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 3;
+        double result = (num1 + num2) * 2.0; // Example calculation, replace with your own function or code
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + result);
+    }
+}
