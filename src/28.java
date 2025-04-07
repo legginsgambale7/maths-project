a748880cfad45f8cca268d77cced7218e4081444
legginsgambale7 <legginsgambale7@gmail.com>
2025-04-07 14:32:25 +0000

    Add 28.java

---
+++ src/28.java
@@ -0,0 +1,12 @@
+public class MathOperations {
+    public static void main(String[] args) {
+        // Example of basic arithmetic operations: addition, subtraction, multiplication, division
+        double num1 = 5.0;
+        double num2 = 3.0;
+
+        System.out.println("Addition: " + (num1 + num2));
+        System.out.println("Subtraction: " + (num1 - num2));
+        System.out.println("Multiplication: " + (num1 * num2));
+        System.out.println("Division: " + (double) num1 / num2);
+    }
+}
