2f99843d0d9ddb4c51215554afa922acf2808183
legginsgambale7 <legginsgambale7@gmail.com>
2025-04-09 06:59:51 +0000

    Add 30.java

---
+++ src/30.java
@@ -0,0 +1,13 @@
+public class MathOperations {
+    public static void main(String[] args) {
+        // Example of performing basic arithmetic operations
+        int result1 = 5 + 3;
+        System.out.println("The sum is: " + result1);
+
+        double result2 = 7.5 - 4.0;
+        System.out.println("The difference is: " + result2);
+
+        int result3 = 10 % 3;
+        System.out.println("The remainder is: " + result3);
+    }
+}
