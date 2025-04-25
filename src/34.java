3feb980ee2b00a27449bd5b7dc488eeed0e4eb7e
legginsgambale7 <legginsgambale7@gmail.com>
2025-04-25 20:30:20 +0000

    Add 34.java

---
+++ src/34.java
@@ -0,0 +1,10 @@
+public class MathOperations {
+    public static void main(String[] args) {
+        int result = add(3, 4);
+        System.out.println("The sum is: " + result);
+    }
+
+    private static int add(int a, int b) {
+        return a + b;
+    }
+}
