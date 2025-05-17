1a1fe3cd7ae5da505eb7b039fd16782fdf1ddcac
legginsgambale7 <legginsgambale7@gmail.com>
2025-05-17 05:52:58 +0000

    Add 37.java

---
+++ src/37.java
@@ -0,0 +1,9 @@
+public class MathOperations {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+}
