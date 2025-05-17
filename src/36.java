a336037e4dee18ad3a134abe61cf46b1c055ec23
legginsgambale7 <legginsgambale7@gmail.com>
2025-05-17 04:11:35 +0000

    Add 36.java

---
+++ src/36.java
@@ -0,0 +1,19 @@
+public class MathematicalOperations {
+
+    public static void main(String[] args) {
+        // Addition operation
+        System.out.println("The result of addition is: " + (3 + 5));
+
+        // Subtraction operation
+        int subtraction = 10 - 2;
+        System.out.println("Subtraction result: " + subtraction);
+
+        // Multiplication operation
+        int multiplication = 4 * 6;
+        System.out.println("Multiplication result: " + multiplication);
+
+        // Division operation
+        double division = 8 / 2.5;
+        System.out.println("Division result: " + division);
+    }
+}
