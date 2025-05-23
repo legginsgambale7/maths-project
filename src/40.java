d331b839e14afa2b940cd62bd0b20c6f29ee2d05
legginsgambale7 <legginsgambale7@gmail.com>
2025-05-23 06:41:54 +0000

    Add 40.java

---
+++ src/40.java
@@ -0,0 +1,13 @@
+public class MathOperations {
+    public static void main(String[] args) {
+        // Example of adding two numbers
+        int num1 = 5;
+        int num2 = 3;
+        int sum = num1 + num2;
+        System.out.println("The sum is: " + sum);
+        
+        // Example of subtracting two numbers
+        int diff = 8 - 4;
+        System.out.println("The difference is: " + diff);
+    }
+}
