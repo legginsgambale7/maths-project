327f680a75933a60f54b5c684a3adec083000716
legginsgambale7 <legginsgambale7@gmail.com>
2025-05-26 21:29:33 +0000

    Add 42.java

---
+++ src/42.java
@@ -0,0 +1,11 @@
+public class MathOperations {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 3;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + (num1 + num2));
+        System.out.println("The difference between " + num1 + " and " + num2 + " is: " + (num1 - num2));
+        System.out.println("The product of " + num1 + " and " + num2 + " is: " + (num1 * num2));
+        System.out.println("The quotient of " + num1 + " and " + num2 + " is: " + (num1 / num2));
+    }
+}
